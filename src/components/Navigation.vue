656ee74140d8569e84df0b972803853c1b2f36cc
floatingbug <sieber.thomas@yahoo.com>
2024-07-23 11:21:42 +0200

    first commit

---
+++ src/components/Navigation.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {ref} from "vue";
+import Menubar from "primevue/menubar";
+const breakpoint = ref("500px");
+const items = ref([
+	{
+		label: "Home"
+	}
+]);
+</script>
+
+
+<template>
+	<Menubar :model="items" :breakpoint="breakpoint" :pt="{ 
+		root: { 
+		},
+		end: {
+			style: 'background-color: red'
+		},
+	}">
+		<template #start>start here</template>
+
+		<template #item="{item}">
+			<span>{{item.label}}</span>
+		</template>
+
+		<template #end>end</template>
+	</Menubar>
+</template>
+
+
+<style scoped>
+.menubar-container {
+}
+.end {
+	background-color: red;
+	color: red;
+}
+</style>
